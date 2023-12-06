116ddf6c4d0539e8d083497526f6207c289dae87
Heshan Jayasinghe <heshan@Heshans-MacBook-Pro.local>
2023-12-06 16:56:50 +0800

    add jsx app and student  note1

---
+++ src/test1/App.js
@@ -0,0 +1,25 @@
+import Student from "./Student";
+function App() {
+    let name = "Hesh";
+    let bool = true;
+    let arr = ["A", "B", "C"]
+    let obj = { name: "Jay" }
+    // we can show only string and numbers
+    function printf() {
+        return "JAY"
+    };
+
+    return (
+        <>
+            {/* <h1>Welcome to React with {name} {bool} {arr} {printf()}</h1>
+            <h2>Thanks</h2> */}
+            <Student/>
+        </>
+    );
+    //jsx only support only one parent, you cannot <h1></h1><h1></h1>
+    // jsx support Fragments
+    // jsx need camelcase for attributes otherwise it gives warnings
+    // attribute values we need to use curlybrase in jsx
+}
+
+export default App;
---
+++ src/test1/Student.js
@@ -0,0 +1,8 @@
+const Student = () => {
+    return <div>
+        <h1>Hesh</h1>
+        <h2>Age: 25</h2>
+    </div>
+}
+
+export default Student;
